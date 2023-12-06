fa073c60adb3d0c9577eeb7e95ead48b3eb5943d
Vlad Ryzhikov <v.ryzhikov@htanywhere.com>
2023-12-06 23:20:09 +0530

    fix gitignore

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,48 @@
+import { RuleSetRule } from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildSvgLoader } from './loaders/buildSvgLoader';
+
+export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                ],
+            },
+        },
+    };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
+
+    const svgLoader = buildSvgLoader();
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    };
+
+    const cssLoader = buildCssLoader(isDev);
+
+    return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
+}
